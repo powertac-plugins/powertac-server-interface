3a4df64c2f0de4c04ac86fcba66816ed7af5ae9f
John Collins <jcollins@cs.umn.edu>
2011-02-09 15:58:48 -0600

    #99 - new server interface plugin

---
+++ grails-app/conf/BuildConfig.groovy
@@ -0,0 +1,53 @@
+grails.project.class.dir = "target/classes"
+grails.project.test.class.dir = "target/test-classes"
+grails.project.test.reports.dir = "target/test-reports"
+//grails.project.war.file = "target/${appName}-${appVersion}.war"
+
+// local plugin dependencies --
+// replace with standard install-plugin before release
+grails.plugin.location.PowertacCommon = "../powertac-common"
+
+grails.project.dependency.resolution = {
+  // inherit Grails' default dependencies
+  inherits("global") {
+    // uncomment to disable ehcache
+    // excludes 'ehcache'
+  }
+  log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+  repositories {
+    grailsPlugins()
+    grailsHome()
+    grailsCentral()
+
+    // uncomment the below to enable remote dependency resolution
+    // from public Maven repositories
+    mavenLocal()
+    mavenCentral()
+    //mavenRepo "http://snapshots.repository.codehaus.org"
+    //mavenRepo "http://repository.codehaus.org"
+    //mavenRepo "http://download.java.net/maven/2/"
+    //mavenRepo "http://repository.jboss.com/maven2/"
+
+    // project repositories
+    mavenRepo "http://ibwstinger.iw.uni-karlsruhe.de/artifactory/plugins-release-local/"
+    mavenRepo "http://ibwstinger.iw.uni-karlsruhe.de/artifactory/plugins-snapshot-local/"
+    mavenRepo "http://ibwstinger.iw.uni-karlsruhe.de/artifactory/libs-release-local/"
+  }
+  dependencies {
+    // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
+
+    // runtime 'mysql:mysql-connector-java:5.1.13'
+  }
+}
+
+def repositoryUserName = '[your powertac artifactory user]'
+def repositoryPassword = '[your powertac artifactory pw]'
+
+grails.project.dependency.distribution = {
+  remoteRepository(id: "powertacPlugins", url: "http://ibwstinger.iw.uni-karlsruhe.de/artifactory/plugins-release-local/") {
+    authentication username: repositoryUserName, password: repositoryPassword
+  }
+  remoteRepository(id: "powertacPluginSnapshots", url: "http://ibwstinger.iw.uni-karlsruhe.de/artifactory/plugins-snapshot-local/") {
+    authentication username: repositoryUserName, password: repositoryPassword
+  }
+}
